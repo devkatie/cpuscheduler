68c2c8b975d7dbca9cff14feac985247b6d48098
Danny Fayaud <dfayaud@gmail.com>
2019-11-04 17:27:15 -0500

    application pushed

--- CPUScheduler/src/controller/ProjectController.java
+++ CPUScheduler/src/controller/ProjectController.java
@@ -1,13 +1,14 @@
 package controller;
 
 import model.ProcessBag;
+import view.View;
 
 public class ProjectController {
 	private ProcessBag processbag;
 	private SubController1 subController1;
 	private SubController2 subController2;
 	private SubController3 subController3;
-	private KatiesScene katiesScene;
+	private View view;
 	
 	
 	public ProjectController(ProcessBag processBag, KatiesScene katiesScene) {
@@ -15,8 +16,7 @@ public class ProjectController {
 		this.subController1 = new SubController1(processBag, katiesScene);
 		this.subController2 = new SubController2(processBag, katiesScene);
 		this.subController3 = new SubController3(processBag, katiesScene);
-		
+
 	}
-	
 	
 }
